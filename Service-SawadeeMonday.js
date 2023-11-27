b7d617cb9f781e8804ad2f44f1f6014798f10e70
siriroh kittiwuttikul <s55160304@gmail.com>
2023-11-27 10:33:50 +0700

    Backup สามารถเพิ่ม Player ผู้เล่นได้ - กำลังเขียน ทำการเซฟรูปและเปลี่ยนชื่อไฟล์

--- Service-SawadeeMonday.js
+++ Service-SawadeeMonday.js
@@ -3,11 +3,14 @@ const mysql = require('mysql');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 app.use(cors({
     origin:'*'
 }));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 const con = mysql.createConnection({
     host:"localhost",
     port:8889,
@@ -54,6 +57,35 @@ app.get('/Type_player',(req,res) => {
     con.query("Select * From Type_player",function(err,result,field){
         res.json(result);
    });
+});// API ดึงประเภทมือขึ้นมา
+
+app.post('/Add_player',(req,res) =>{
+
+    //res.json(true);
+    const sql = "INSERT INTO Player SET ?";
+    const value = {
+        Player_name:req.body.Name,
+        Player_tel:req.body.Phone,
+        Type_id:req.body.Type,
+        Player_photo:req.body.Photo,
+        Player_registertime:new Date()
+    };
+   
+    con.query(sql,value,function(err,result){
+        try{
+            res.json(result.insertId);
+        }catch(err){
+            console.log(err);
+        }
+        console.log("Number of recode inserted:" + result.insertId);
+    });
+});//เพิ่มผู้เล่นใหม่เข้ามาในระบบ
+
+app.get('/Allplayer',(req,res) =>{
+    console.log("Hello Function get All Player");
+    con.query("Select * From Player",function(err,result,field){
+        res.json(result);
+    });
 });
 
 app.listen('7777',() =>{
